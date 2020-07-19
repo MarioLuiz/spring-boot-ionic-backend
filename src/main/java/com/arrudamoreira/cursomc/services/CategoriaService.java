70f503d8cd096c002b4a36f7fcd6a7590faa2483
Mario Luiz <marioluiz.arruda@gmail.com>
2020-07-19 18:09:44 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/arrudamoreira/cursomc/services/CategoriaService.java
@@ -0,0 +1,28 @@
+package com.arrudamoreira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arrudamoreira.cursomc.domain.Categoria;
+import com.arrudamoreira.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+//	Maneira Antiga de usar método Find, usada nas versões Spring 1.x.x com Java 8	
+//	public Categoria find(Integer id) {
+//		Categoria obj = repo.findOne(id);
+//		return obj;
+//	}
+	
+	// Atualização do método para Spring 2.x.x com Java 11
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
