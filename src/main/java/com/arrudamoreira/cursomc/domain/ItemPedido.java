2175bea4b3549bdd4f5872c2b56e23903abb1d1c
Mario Luiz <marioluiz.arruda@gmail.com>
2020-08-03 20:17:57 -0400

    Subtotal e total do pedido

--- src/main/java/com/arrudamoreira/cursomc/domain/ItemPedido.java
+++ src/main/java/com/arrudamoreira/cursomc/domain/ItemPedido.java
@@ -21,7 +21,11 @@ public class ItemPedido implements Serializable{
 	
 	public ItemPedido() {
 	}
-
+	
+	public double getSubTotal() {
+		return (preco - desconto ) * quantidade;
+	}
+	
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
